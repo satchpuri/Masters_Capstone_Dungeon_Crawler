961382c5d9f721ed0d424cd626d5c6f6fcdb3d22
Aiden Melendez <itm5305@rit.edu>
2018-09-03 15:49:00 -0400

    working update from merge

---
+++ Capstone Dungeon Crawler/Assets/Scripts/PickableLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableLight : MonoBehaviour {
+
+    public bool switchedOn = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(switchedOn)
+        {
+            for(int i=0; i < transform.childCount; i++)
+            {
+                if(transform.GetChild(i).gameObject.tag == "LightSource")
+                {
+                    transform.GetChild(i).gameObject.SetActive(true);
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (transform.GetChild(i).gameObject.tag == "LightSource")
+                {
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
