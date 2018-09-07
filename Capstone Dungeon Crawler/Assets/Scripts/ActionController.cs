fe94ff170009c2b86d387b72df82893625f3740c
Jishnu Girish <jishnugirish@live.in>
2018-09-07 17:56:56 -0400

    Inventory scroll

--- Capstone Dungeon Crawler/Assets/Scripts/ActionController.cs
+++ Capstone Dungeon Crawler/Assets/Scripts/ActionController.cs
@@ -6,8 +6,9 @@ public class ActionController : MonoBehaviour {
 
     Transform LeftHand;
     Transform RightHand;
-	// Use this for initialization
-	void Start () {
+    Pickup nextItem;
+    // Use this for initialization
+    void Start () {
         LeftHand = transform.Find("LeftHand");
         RightHand = transform.Find("RightHand");
 	}
@@ -16,15 +17,19 @@ public class ActionController : MonoBehaviour {
 	void Update () {
         if (Input.GetKeyDown(KeyCode.G))
         {
-            if(LeftHand.childCount != 0)
+            if (LeftHand.childCount != 0)
             {
-                LeftHand.DetachChildren();
-                //print("left drop");
+               var drop = DropLeftItem();
+                //remove from inventory
+                Inventory.instance.RemoveItem(drop.GetComponent<Pickup>());
+                Inventory.instance.currentIndex = 0;
             }
             else if (RightHand.childCount != 0)
             {
-                RightHand.DetachChildren();
-                //print("right drop");
+                var drop = DropRightItem();
+                //remove from inventory
+                Inventory.instance.RemoveItem(drop.GetComponent<Pickup>());
+                Inventory.instance.currentIndex = 0;
             }
         }
 
@@ -32,7 +37,9 @@ public class ActionController : MonoBehaviour {
         {
             if(LeftHand.childCount != 0)
             {
-                LeftHand.GetChild(0).GetComponent<PickableLight>().switchedOn = !LeftHand.GetChild(0).GetComponent<PickableLight>().switchedOn;
+                LeftHand.GetChild(0).GetComponent<Pickup>().UseItem();
+                if(LeftHand.GetChild(0).GetComponent<PickableLight>())
+                    LeftHand.GetChild(0).GetComponent<PickableLight>().switchedOn = !LeftHand.GetChild(0).GetComponent<PickableLight>().switchedOn;
             }
         }
 
@@ -40,31 +47,164 @@ public class ActionController : MonoBehaviour {
         {
             if (RightHand.childCount != 0)
             {
-                RightHand.GetChild(0).GetComponent<PickableLight>().switchedOn = !RightHand.GetChild(0).GetComponent<PickableLight>().switchedOn;
+                LeftHand.GetChild(0).GetComponent<Pickup>().UseItem();
+                if (RightHand.GetChild(0).GetComponent<PickableLight>())
+                    RightHand.GetChild(0).GetComponent<PickableLight>().switchedOn = !RightHand.GetChild(0).GetComponent<PickableLight>().switchedOn;
             }
+        }
+
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        if(scroll < 0)
+        {
+            EquipNextInventoryItem();
+        }
+        if (scroll > 0)
+        {
+            EquipBackInventoryItem();
         }
     }
 
+
+    //Equip next inventory Item
+    void EquipNextInventoryItem()
+    {
+        GameObject droppedItem = DropLeftItem();
+        if (droppedItem != null)
+        {
+            droppedItem.SetActive(false);
+        }
+            // Left Hand holds new item
+            int index = Inventory.instance.NextIndex();
+            nextItem = Inventory.instance.inventoryItems[index];
+            nextItem.prefab.transform.SetParent(LeftHand);
+            nextItem.prefab.transform.position = LeftHand.position;
+            nextItem.prefab.transform.localRotation = Quaternion.identity;
+            nextItem.prefab.gameObject.GetComponent<Pickup>().isHolding = true;
+            nextItem.prefab.gameObject.GetComponent<SphereCollider>().enabled = false;
+            nextItem.prefab.SetActive(true);
+        
+        // Right hand holds next item
+        droppedItem = DropRightItem();
+        if (droppedItem != null)
+        {
+            droppedItem.SetActive(false);
+        }
+            nextItem = Inventory.instance.inventoryItems[Inventory.instance.NextIndex()];
+            nextItem.prefab.transform.SetParent(RightHand);
+            nextItem.prefab.transform.position = RightHand.position;
+            nextItem.prefab.transform.localRotation = Quaternion.identity;
+            nextItem.prefab.gameObject.GetComponent<Pickup>().isHolding = true;
+            nextItem.prefab.gameObject.GetComponent<SphereCollider>().enabled = false;
+            nextItem.prefab.SetActive(true);
+    }
+
+    //Equip previous inventory Item
+    void EquipBackInventoryItem()
+    {
+        GameObject droppedItem = DropRightItem();
+        if (droppedItem != null)
+        {
+            droppedItem.SetActive(false);
+        }
+        // Right hand holds next item
+        nextItem = Inventory.instance.inventoryItems[Inventory.instance.NextIndex()];
+        nextItem.prefab.transform.SetParent(RightHand);
+        nextItem.prefab.transform.position = RightHand.position;
+        nextItem.prefab.transform.localRotation = Quaternion.identity;
+        nextItem.prefab.gameObject.GetComponent<Pickup>().isHolding = true;
+        nextItem.prefab.gameObject.GetComponent<SphereCollider>().enabled = false;
+        nextItem.prefab.SetActive(true);
+
+
+        
+        droppedItem = DropLeftItem();
+        if (droppedItem != null)
+        {
+            droppedItem.SetActive(false);
+        }
+        
+        // Left Hand holds new item
+        int index = Inventory.instance.NextIndex();
+        nextItem = Inventory.instance.inventoryItems[index];
+        nextItem.prefab.transform.SetParent(LeftHand);
+        nextItem.prefab.transform.position = LeftHand.position;
+        nextItem.prefab.transform.localRotation = Quaternion.identity;
+        nextItem.prefab.gameObject.GetComponent<Pickup>().isHolding = true;
+        nextItem.prefab.gameObject.GetComponent<SphereCollider>().enabled = false;
+        nextItem.prefab.SetActive(true);
+    }
+
+    // Drop Item in the left hand
+    GameObject DropLeftItem()
+    {
+        if (LeftHand.childCount != 0)
+        {
+            GameObject droppedItem;
+            foreach (SphereCollider childCollider in LeftHand.GetComponentsInChildren<SphereCollider>())
+            {
+                childCollider.enabled = true;
+            }
+            droppedItem = LeftHand.GetChild(0).gameObject;
+            LeftHand.DetachChildren();
+            return droppedItem;
+        }
+        return null;
+    }
+
+    //Drop Item in the right hand
+    GameObject DropRightItem()
+    {
+        if (RightHand.childCount != 0)
+        {
+            GameObject droppedItem;
+            foreach (SphereCollider childCollider in RightHand.GetComponentsInChildren<SphereCollider>())
+            {
+                childCollider.enabled = true;
+            }
+            droppedItem = RightHand.GetChild(0).gameObject;
+            RightHand.DetachChildren();
+            return droppedItem;
+        }
+        return null;
+    }
+
+    // Pickup Items
     public void OnTriggerStay(Collider other)
     {
-        if(Input.GetKey(KeyCode.E))     // Pickup objects
+        if(Input.GetKeyUp(KeyCode.E))     // Pickup objects
         {
             if(other.gameObject.GetComponent<Pickup>() != null)
             {
                 if(LeftHand.childCount == 0)
                 {
-                    other.transform.SetParent(LeftHand);
-                    other.transform.position = LeftHand.position;
-                    other.transform.localRotation = Quaternion.identity;
+                    if (Inventory.instance.AddItem(other.gameObject.GetComponent<Pickup>()))
+                        {
+                            other.transform.SetParent(LeftHand);
+                            other.transform.position = LeftHand.position;
+                            other.transform.localRotation = Quaternion.identity;
+                            other.gameObject.GetComponent<Pickup>().isHolding = true;
+                            other.gameObject.GetComponent<SphereCollider>().enabled = false;
+                            print("holding item\n");
+                        }
                 }
                 else if(RightHand.childCount == 0)
                 {
-                    other.transform.SetParent(RightHand);
-                    other.transform.position = RightHand.position;
-                    other.transform.localRotation = Quaternion.identity;
+                    if (Inventory.instance.AddItem(other.gameObject.GetComponent<Pickup>()))
+                    {
+                        other.transform.SetParent(RightHand);
+                        other.transform.position = RightHand.position;
+                        other.transform.localRotation = Quaternion.identity;
+                        other.gameObject.GetComponent<Pickup>().isHolding = true;
+                        other.gameObject.GetComponent<SphereCollider>().enabled = false;
+                        print("holding item\n");
+                    }
                 }
-                other.gameObject.GetComponent<Pickup>().isHolding = true;
-                print("picked up\n");
+                else
+                {
+                    if(Inventory.instance.AddItem(other.gameObject.GetComponent<Pickup>()))
+                        other.gameObject.SetActive(false);                                      // disable if picked up
+                }
+               
             }
 
         }
--- Capstone Dungeon Crawler/Assets/Scripts/Pickup.cs
+++ Capstone Dungeon Crawler/Assets/Scripts/Pickup.cs
@@ -6,13 +6,20 @@ public class Pickup : MonoBehaviour {
 
     public string name;
     public bool isHolding = false;
+    public  GameObject prefab;
+
 	// Use this for initialization
 	void Start () {
-		
+        prefab = this.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public virtual void UseItem()
+    {
+        print("Item in use");
+    }
 }
