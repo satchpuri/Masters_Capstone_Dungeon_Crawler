a4add7c2c6234d324e5dd2a774e1a0d82dddfbe1
Jishnu Girish <jishnugirish@live.in>
2018-09-07 10:27:57 -0400

    character models

--- Capstone Dungeon Crawler/Assets/Scripts/Controller.cs
+++ Capstone Dungeon Crawler/Assets/Scripts/Controller.cs
@@ -9,11 +9,12 @@ public class Controller : MonoBehaviour {
 	Rigidbody rigidbody;
 	Camera viewCamera;
 	Vector3 velocity;
-
-	void Start () {
+    Animator animator;
+    void Start () {
 		rigidbody = GetComponent<Rigidbody> ();
 		viewCamera = Camera.main;
-	}
+        animator = GetComponent<Animator>();
+    }
 
 	void Update () {
 		Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, viewCamera.transform.position.y));
@@ -23,9 +24,18 @@ public class Controller : MonoBehaviour {
 		// reset scene
 		if (Input.GetKeyDown (KeyCode.LeftShift))
 			SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) )
+        {
+            animator.SetBool("Run", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Run", false);
+        }
     }
 
 	void FixedUpdate() {
 		rigidbody.MovePosition (rigidbody.position + velocity * Time.fixedDeltaTime);
-	}
+    }
 }
