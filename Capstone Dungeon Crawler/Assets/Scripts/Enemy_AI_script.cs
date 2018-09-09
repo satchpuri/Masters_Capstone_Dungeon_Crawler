653f7d80bb927046ef640e963580fc28341c41ba
tomboyanushka <anushkanair20@gmail.com>
2018-09-09 19:53:17 -0400

    AI navmesh pit system works

--- Capstone Dungeon Crawler/Assets/Scripts/Enemy_AI_script.cs
+++ Capstone Dungeon Crawler/Assets/Scripts/Enemy_AI_script.cs
@@ -6,41 +6,69 @@ using UnityEngine.AI;
 public class Enemy_AI_script : MonoBehaviour
 {
     NavMeshAgent enemyAgent;
-    Ray enemyVision;
-    RaycastHit rayHit;
+    public Transform lightSource;
+    public Transform Player;
+
     bool pursuePlayer = false;
     bool pursueLight = false;
+    bool attackPlayer = false;
+    bool lightIsOn = true;
 
-	// Use this for initialization
-	void Start ()
+    public float enemyVision = 10f;
+
+    // Use this for initialization
+    void Start()
     {
-        enemyAgent = GetComponent<NavMeshAgent>();
-		
-	}
+        enemyAgent = GetComponentInChildren<NavMeshAgent>();
+
+    }
 
     // Update is called once per frame
     void Update()
     {
-        enemyVision = new Ray(transform.position, transform.forward * 10);
-        Debug.DrawRay(transform.position, transform.forward * 10, Color.blue);
 
-        if (Physics.Raycast(transform.position, transform.forward, out rayHit, 30))
+        float distanceToLight = Vector3.Distance(lightSource.position, transform.position);
+
+        if (lightIsOn)
         {
-            Debug.DrawRay(transform.position, Vector3.up, Color.red);
-
-            if (rayHit.transform.tag == "LightSource")
+            if (distanceToLight <= enemyVision)
             {
-
-                Debug.Log("I see the light!");
-                pursueLight = true;
                 PursueLight();
+                Debug.Log("I see the light!");
             }
+        }
+       
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            pursuePlayer = true;
+            Debug.Log("I will eat the child!");
         }
     }
 
     void PursueLight()
     {
         //seek lights position
+        enemyAgent.SetDestination(lightSource.position);
     }
+
+    void PursuePlayer()
+    {
+
+    }
+
+    void AttackPlayer()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, enemyVision);
+    }
+
 }
