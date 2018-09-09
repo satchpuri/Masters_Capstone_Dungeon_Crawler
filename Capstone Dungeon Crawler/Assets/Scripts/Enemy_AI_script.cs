fe12164707eb8f72b8647f76ffe6b93d2eb59d58
tomboyanushka <32500051+tomboyanushka@users.noreply.github.com>
2018-09-09 13:01:08 -0400

    AI progress

--- Capstone Dungeon Crawler/Assets/Scripts/Enemy_AI_script.cs
+++ Capstone Dungeon Crawler/Assets/Scripts/Enemy_AI_script.cs
@@ -1,16 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class Enemy_AI_script : MonoBehaviour {
+public class Enemy_AI_script : MonoBehaviour
+{
+    NavMeshAgent enemyAgent;
+    Ray enemyVision;
+    RaycastHit rayHit;
+    bool pursuePlayer = false;
+    bool pursueLight = false;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+        enemyAgent = GetComponent<NavMeshAgent>();
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyVision = new Ray(transform.position, transform.forward * 10);
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.blue);
+
+        if (Physics.Raycast(transform.position, transform.forward, out rayHit, 30))
+        {
+            Debug.DrawRay(transform.position, Vector3.up, Color.red);
+
+            if (rayHit.transform.tag == "LightSource")
+            {
+
+                Debug.Log("I see the light!");
+                pursueLight = true;
+                PursueLight();
+            }
+
+        }
+    }
+
+    void PursueLight()
+    {
+        //seek lights position
+    }
 }
